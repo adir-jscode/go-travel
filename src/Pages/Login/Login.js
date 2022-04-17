e712831f393d1821831fc44bcc4a40cfc580262e
adir-jscode <adir.jscode@gmail.com>
2022-04-17 20:48:20 +0600

    login and register ongoing

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -10,10 +10,17 @@ const Login = () => {
   const handleRegistration = () => {
     navigate("/register");
   }
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    console.log(email, password);
+  }
     return (
       <div className="container w-50 mx-auto mt-5">
         <h1 className="text-info text-center">Login</h1>
-      <Form>
+      <Form onSubmit={handleLogin}>
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Email address</Form.Label>
     <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
@@ -24,7 +31,7 @@ const Login = () => {
     <Form.Control ref={passwordRef} type="password" placeholder="Password" />
   </Form.Group>
   <Button variant="primary" type="submit">
-    Submit
+    Login
           </Button>
           <p onClick={handleRegistration} className="text-center">New to Go Travel? <span className="text-info">Create a new account</span> </p>
 </Form>
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,9 +1,48 @@
-import React from 'react';
+
+import React, { useRef } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
+    const nameRef = useRef('');
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate= useNavigate();
+
+    const handleLogin = () => {
+        navigate("/login")
+    }
+    const handleRegister = (event) => {
+        event.preventDefault();
+        const name = nameRef.current.value;
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        console.log(name, email, password);
+    }
     return (
-        <div>
-            <h1>Welcome to go travel</h1>
+        <div className="container w-50 mx-auto mt-5">
+            <h1 className="text-info text-center">Register</h1>
+            <Form onSubmit={handleRegister}>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Name</Form.Label>
+    <Form.Control ref={nameRef} type="text" placeholder="Enter your name" />
+
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
+
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control ref={passwordRef} type="password" placeholder="Password" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Register
+                </Button>
+                <p onClick={handleLogin} className="text-center">Already have an account? <span className="text-info">Please Login</span> </p>
+</Form>
         </div>
     );
 };
