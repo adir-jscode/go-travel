ec8a2a7a155a02726e18f628dcff51d88c1f1604
adir-jscode <adir.jscode@gmail.com>
2022-04-18 17:36:30 +0600

    about page done

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            about me
+        <div className="container">
+            <h3>Name : Sarafat Ali Adir </h3>
+            <p>I am always stay open-minded and focused. I train hard & train smart. For me, the older I get, the smarter I have to train also, because the recovery time is longer. Work on everything,become a well-rounded fighter,i don't just be good at one thing,i will be good at everything.</p>
         </div>
     );
 };
